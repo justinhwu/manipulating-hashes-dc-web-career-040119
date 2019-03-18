7dd97c3ccbd66d46cd244e9dd2e14770656cd4b6
Justin Wu <justinhwu95@gmail.com>
2019-03-18 23:34:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 first_challenge[:contacts]["Freddy Mercury"][favorite_icecream_flavors].pop(1)
-return first_challenge
+return contacts
 
   #remember to return your newly altered contacts hash!
   contacts
